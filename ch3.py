c89cb82e2b73d2fd8a84c9a0205cf004fde33c80
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 13:01:10 +0900

    added sigmoid function

--- ch3.py
+++ ch3.py
@@ -101,6 +101,27 @@ plt.legend(loc="upper left")
 
 plt.show()
 
+print(50 * "=")
+print("Section: Logistic regression intuition and conditional probabilities")
+print(50 * "-")
+
+def sigmoid(z):
+  return 1.0 / (1.0 + np.exp(-z))
+
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+
+plt.plot(z, phi_z)
+plt.axvline(0.0, color="k")
+plt.ylim(-0.1, 1.1)
+plt.xlabel("z")
+plt.ylabel("$\phi (z)$")
+
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+
+plt.show()
 
 
 
