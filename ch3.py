85de28d982ea34a5e359fcc73d6dd44659e81b97
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 18:27:52 +0900

    Added 決定木の三つ

--- ch3.py
+++ ch3.py
@@ -243,6 +243,7 @@ plt.legend(loc="best")
 plt.show()
 
 
+
 print(50 * "=")
 print("Section: Using the kernel trick to find separating hyperplanes"
         "inhigher dimensional space")
@@ -273,6 +274,53 @@ plot_decision_regions(X_combined_std, y_combined,
 plt.xlabel("petal length [standardized]")
 plt.ylabel("petal width [standardized]")
 plt.legend(loc="upper left")
+
+plt.show()
+
+
+
+print(50 * "=")
+print("Section: Decision tree learning")
+print(50 * "-")
+
+#ジニ不順度の関数を定義
+def gini(p):
+  return p * (1 - p) + (1 - p) * (1 - (1 - p))
+
+#エントロピーの関数を定義
+def entropy(p):
+  return - p * np.log2(p) - (1 - p) * np.log2((1 - p))
+
+#分類誤差の関数を定義
+def error(p):
+  return 1 - np.max([p, 1 - p])
+#確率を表す配列を生成（０から0.99まで0.01刻み）
+x = np.arange(0.0, 1.0, 0.01)
+
+#配列の値を元にエントロピー、分類誤差を計算
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+
+fig = plt.figure()
+ax = plt.subplot(111)
+#エントロピー（２種）、ジニ不順度、分類誤差のそれぞれをループ処理
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                          ["Entropy", "Entropy (scaled)",
+                            "Gini Impurity", "Misclassification Error"],
+                            ["-", "-", "--", "-."],
+                            ["black", "lightgray", "red", "green", "cyan"]):
+  line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+#凡例の設定（中央の上に配置）
+ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+#２本の水平の破線を引く
+ax.axhline(y=0.5, linewidth=1, color="k", linestyle="--")
+ax.axhline(y=1.0, linewidth=1, color="k", linestyle="--")
+plt.ylim([0, 1.1])
+plt.xlabel("p(i=1)")
+plt.ylabel("Impurity Index")
 
 plt.show()
 
