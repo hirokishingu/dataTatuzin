052046776a3e56a33ab0d81930f6bc020c994598
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 10:29:57 +0900

    Added ADALINESGD

--- ch2.py
+++ ch2.py
@@ -207,6 +207,98 @@ plt.ylabel("Sum-suared-error")
 plt.show()
 
 
+print(50 * "=")
+print("Large scale machine learning and stochastic gradient descent")
+print(50 * "-")
+
+class AdalineSGD(object):
+  def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+    self.eta = eta
+    self.n_iter = n_iter
+    self.w_initialized = False
+    self.shuffle = shuffle
+    if random_state:
+      np.random.seed(random_state)
+
+  def fit(self, X, y):
+    self._initialize_weights(X.shape[1])
+    self.cost_ = []
+    for i in range(self.n_iter):
+      if self.shuffle:
+        X, y = self._shuffle(X, y)
+      cost = []
+      for xi, target in zip(X, y):
+        cost.append(self._update_weights(xi, target))
+      avg_cost = sum(cost) / len(y)
+      self.cost_.append(avg_cost)
+    return self
+
+  def partial_fit(self, X, y):
+    """重みを再初期化することなくトレーニングデータに適合せる"""
+    if not self.w_initialized:
+      self._initialize_weights(X.shape[1])
+    if y.ravel().shape[0] > 1:
+      for xi, target in zip(X, y):
+        self._update_wieghts(xi, target)
+    else:
+      self._update_weights(X, y)
+    return self
+
+  def _shuffle(self, X, y):
+    """トレーニングデータをシャッフル"""
+    r = np.random.permutation(len(y))
+    return X[r], y[r]
+
+  def _initialize_weights(self, m):
+    """重みを０にする初期化"""
+    self.w_ = np.zeros(1 + m)
+    self.w_initialized = True
+
+  def _update_weights(self, xi, target):
+    """ADALINEの学習規則を用いて重みを更新"""
+    output = self.net_input(xi)
+    error = (target - output)
+    self.w_[1:] += self.eta * xi.dot(error)
+    self.w_[0] += self.eta * error
+    cost = 0.5 * error**2
+    return cost
+
+  def net_input(self, X):
+    """総入力を計算"""
+    return np.dot(X, self.w_[1:]) + self.w_[0]
+
+  def activation(self, X):
+    """線形活性化関数の出力を計算"""
+    return self.net_input(X)
+
+  def predict(self, X):
+    """1ステップ後のクラスラベルを返す"""
+    return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title("Adaline - Stochastic Gradient Descent")
+plt.xlabel("sepal length [standardized]")
+plt.ylabel("petal length [standardized]")
+plt.legend(loc="upper left")
+
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker="o")
+plt.xlabel("Epochs")
+plt.ylabel("Average Cost")
+
+plt.show()
+
+ada = ada.partial_fit(X_std[0, :], y[0])
+
+
+
+
+
 
 
 
