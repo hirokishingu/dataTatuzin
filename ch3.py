b109e8d72ded3bb9f48a77bf0ed1ee3970f21a48
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 20:43:42 +0900

    k近傍法のアルゴリズム

--- ch3.py
+++ ch3.py
@@ -353,10 +353,12 @@ print(50 * "=")
 print("Section : Combining weak to strong learners via random forests")
 print(50 * "-")
 
+# エントロピーを指標とするランダムフォレストのインスタンスを生成
 forest = RandomForestClassifier(criterion="entropy",
                                 n_estimators=10,
                                 random_state=1,
                                 n_jobs=2)
+#ランダムフォレストのモデルにトレーニングデータを適合させる
 forest.fit(X_train, y_train)
 
 plot_decision_regions(X_combined, y_combined,
