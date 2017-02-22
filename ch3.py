8a2393bf1df1ee03ee198424874f5706dc42175c
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 20:02:20 +0900

    random forest

--- ch3.py
+++ ch3.py
@@ -349,6 +349,24 @@ plt.show()
 
 
 
+print(50 * "=")
+print("Section : Combining weak to strong learners via random forests")
+print(50 * "-")
+
+forest = RandomForestClassifier(criterion="entropy",
+                                n_estimators=10,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined,
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel("petal length [cm]")
+plt.ylabel("petal width [cm]")
+plt.legend(loc="upper left")
+plt.show()
+
 
 
 
