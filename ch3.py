4079c356615e6ad7fa03b36be3991f4031c34fa0
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 14:39:29 +0900

    added SVMモデルでirisの品種分類

--- ch3.py
+++ ch3.py
@@ -195,7 +195,19 @@ plt.show()
 
 
 
+print(50 * "=")
+print("Section: Dealing with the nonlinearly"
+        "separable case using slach variables")
+print(50 * "-")
 
+svm = SVC(kernel="linear", C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel("petal length [standardized]")
+plt.ylabel("petal width [standardized]")
+plt.legend(loc="upper left")
+plt.show()
 
 
 
