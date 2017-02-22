5a6b050ff49d8c15ac2ca46cb80269cb0118069f
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-23 08:37:22 +0900

    data 色々

--- ch4.py
+++ ch4.py
@@ -38,6 +38,47 @@ print("\n\nExecuting df.isnull().sum():")
 print(df.isnull().sum())
 
 
+print(50 * "=")
+print("Section: Eliminating samples or features with missing values")
+print(50 * "-")
+
+print("\n\nExecuting df.dropna()")
+#欠測値を含む行を削除
+print(df.dropna())
+
+print("\n\nExecuting df.dropna()")
+#欠測値を含む列を削除
+print(df.dropna(axis=1))
+
+print("\n\nExecuting df.dropna(thresh=4)")
+print("(drop rows that have not at least 4 non-NaN values)")
+#非NaN値が４つ未満の行を削除
+print(df.dropna(thresh=4))
+
+print("\n\nExecuting df.dropna(how='all')")
+print("(only drop rows where all columns are NaN")
+#全ての列がNaNである行だけを削除する
+print(df.dropna(how="all"))
+
+print("\n\nExecuting df.dropna(subset=['C'])")
+print("(only drop rows where NaN appear in specific columns (here: 'C'))")
+#特定の列（この場合は”C”）に NaNNあNが含まれている行だけを削除
+print(df.dropna(subset=["C"]))
+
+
+
+print(50 * "=")
+print("Section: Imuputing missing velues")
+print(50 * "-")
+#欠測値補完のインスタンスを生成（平均値補完）
+imr = Imputer(missing_values="NaN", strategy="mean", axis=0)
+#データを適合tekigou
+imr = imr.fit(df)
+#補完を実行
+imputed_data = imr.transform(df.values)
+
+print("Input Array:\n", df.values)
+print("Imputed Data:\n", imputed_data)
 
 
 
