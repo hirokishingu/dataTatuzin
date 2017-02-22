e1a8c8281ff9a4a3f152244d3589d8912b217633
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-22 19:21:09 +0900

    決定木の構築

--- ch3.py
+++ ch3.py
@@ -327,6 +327,48 @@ plt.show()
 
 
 
+print(50 * "=")
+print("Section: Building a decision tree")
+print(50 * "-")
+#エオントロピーを指標とする決定木のインスタンスを生成
+tree = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=0)
+#決定木のモデルにトレーニングデータを適合させる
+tree.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined,
+                      classifier=tree, test_idx=range(105, 150))
+
+plt.xlabel("petal length [cm]")
+plt.ylabel("petal width [cm]")
+plt.legend(loc="upper left")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
