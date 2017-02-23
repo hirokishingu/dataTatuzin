aa26ba81f26b3ac3329d6d59877480f0929a6ca8
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-23 09:14:53 +0900

    wine

--- ch4.py
+++ ch4.py
@@ -151,7 +151,29 @@ print("Pandas get_dummies alternative:\n", df_dummies)
 
 
 
+print(50 * '=')
+print('Section: Partitioning a dataset in training and test sets')
+print(50 * '-')
 
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/wine/wine.data',
+                      header=None)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+
+print('Class labels', np.unique(df_wine['Class label']))
+
+print('\nWine data excerpt:\n\n', df_wine.head())
+
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.3, random_state=0)
 
 
 
