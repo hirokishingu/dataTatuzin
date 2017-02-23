9e6d5407a2a105259498effc330cb054576924f5
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-23 12:20:46 +0900

    added random forest

--- ch4.py
+++ ch4.py
@@ -327,6 +327,52 @@ print("Test accuracy:", knn.score(X_test_std[:, k5], y_test))
 
 
 
+print(50 * "=")
+print("Section: Assessing Feature Importances with Random Forests")
+print(50 * "-")
+
+feat_labels = df_wine.columns[1:]
+
+forest = RandomForestClassifier(n_estimators=10000,
+                                random_state=0,
+                                n_jobs=-1)
+
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+  print("%2d) %-*s %f" % (f + 1, 30,
+                          feat_labels[indices[f]],
+                          importances[indices[f]]))
+
+plt.title("Feature Importances")
+plt.bar(range(X_train.shape[1]),
+        importances[indices],
+        color="lightblue",
+        align="center")
+
+plt.xticks(range(X_train.shape[1]),
+                  feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+
+plt.show()
+
+if Version(sklearn_version) < "0.18":
+  X_selected = forest.transform(X_train, threshold=0.15)
+else:
+  from sklearn.feature_selection import SelectFromModel
+  sfm = SelectFromModel(forest, threshold=0.15, prefit=True)
+  X_selected = sfm.transform(X_train)
+
+X_selected.shape
+
+
+
+
+
+
 
 
 
