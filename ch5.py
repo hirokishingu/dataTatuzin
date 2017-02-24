2c6753ef15d5c3756f499f01643a91beef34ec04
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-24 17:37:31 +0900

    新たしい特徴部分空間の線形判別の選択

--- ch5.py
+++ ch5.py
@@ -171,6 +171,94 @@ X_train_pca = pca.fit_transform(X_train_std)
 print("Explained variance ratio:\n", pca.explained_variance_ratio_)
 
 
+print(50 * "=")
+print("Section: Supervised data compression via linear discriminant analysis")
+print(50 * "-")
+
+np.set_printoptions(precision=4)
+
+mean_vecs = []
+for label in range(1, 4):
+  mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+  print("MV %s: %s\n" % (label, mean_vecs[label - 1]))
+
+
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+  class_scatter = np.zeros((d, d))
+  for row in X_train_std[y_train == label]:
+    row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+    class_scatter += (row - mv).dot((row - mv).T)
+  S_W += class_scatter
+
+print("Within-class scatter matrix: %sx%s" % (S_W.shape[0], S_W.shape[1]))
+
+print('Class label distribution: %s'
+      % np.bincount(y_train)[1:])
+
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+  class_scatter = np.cov(X_train_std[y_train == label].T)
+  S_W += class_scatter
+print("Scaled within-class scatter matrix: %sx%x" % (S_W.shape[0], S_W.shape[1]))
+
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+  n = X_train[y_train == i + 1, :].shape[0]
+  mean_vec = mean_vec.reshape(d, 1)
+  mean_overall = mean_overall.reshape(d, 1)
+  S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+
+print("Between-class scatter matrix: %sx%s" % (S_B.shape[0], S_B.shape[1]))
+
+
+
+print(50 * "=")
+print("Section: Selecting linear discriminants for the new feature subspace")
+print(50 * "-")
+
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+                for i in range(len(eigen_vals))]
+
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+
+print("Eigenvalues in decreasing order:\n")
+for eigen_val in eigen_pairs:
+  print(eigen_val[0])
+
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+
+plt.bar(range(1, 14), discr, alpha=0.5, align="center",
+        label="individual 'discriminability'")
+plt.step(range(1, 14), cum_discr, where="mid",
+          label="cumulative 'discriminability'")
+plt.ylabel("'dicriminability' ratio")
+plt.xlabel("Linear Discriminants")
+plt.ylim([-0.1, 1.1])
+plt.legend(loc="best")
+
+plt.show()
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real,
+              eigen_pairs[1][1][:, np.newaxis].real))
+print("Matrix W:\n", w)
+
+
+
+
+
+
+
+
+
 
 
 
