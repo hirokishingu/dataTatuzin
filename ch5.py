57420071db6ab3a6bd4801c2abd78f56799fc496
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-24 17:04:40 +0900

    LOGiSTIC PCA

--- ch5.py
+++ ch5.py
@@ -77,6 +77,100 @@ plt.legend(loc="best")
 
 plt.show()
 
+print(50 * "=")
+print("Section: Feature Transformation")
+print(50 * "-")
+
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+
+eigen_pairs.sort(reverse=True)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+                eigen_pairs[1][1][:, np.newaxis]))
+print("Matrix W: \n", w)
+
+X_train_pca = X_train_std.dot(w)
+colors = ["r", "b", "g"]
+markers = ["s", "x", "o"]
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+  plt.scatter(X_train_pca[y_train == l, 0],
+              X_train_pca[y_train == l, 1],
+              c=c, label=l, marker=m)
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.legend(loc="lower left")
+
+plt.show()
+print("Dot product:\n", X_train_std[0].dot(w))
+
+
+
+print(50 * "=")
+print("Section: Principal component analysis inscikit-learn")
+print(50 * "-")
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train_std)
+print("Variance explained ratio:\n", pca.explained_variance_ratio_)
+
+plt.bar(range(1, 14), pca.explained_variance_ratio_, alpha=0.5, align="center")
+plt.step(range(1, 14), np.cumsum(pca.explained_variance_ratio_), where="mid")
+plt.ylabel("Explained variance ratio")
+plt.xlabel("Principal components")
+plt.show()
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.show()
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+  markers = ("s", "x", "o", "^", "v")
+  colors = ("red", "blue", "lightgreen", "gray", "cyan")
+  cmap = ListedColormap(colors[:len(np.unique(y))])
+
+  x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+  x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+  xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                          np.arange(x2_min, x2_max, resolution))
+  Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+  Z = Z.reshape(xx1.shape)
+  plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+  plt.xlim(xx1.min(), xx1.max())
+  plt.ylim(xx2.min(), xx2.max())
+
+  for idx, cl in enumerate(np.unique(y)):
+    plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                alpha=0.8, c=cmap(idx),
+                marker=markers[idx], label=cl)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_pca, y_train)
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.legend(loc="lower left")
+
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.legend(loc="lower left")
+
+plt.show()
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+print("Explained variance ratio:\n", pca.explained_variance_ratio_)
+
+
 
 
 
