8896724a3ccd13691e3b5c02d9c6142d9e73d383
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-24 18:26:34 +0900

    Added kernel

--- ch5.py
+++ ch5.py
@@ -271,7 +271,116 @@ plt.legend(loc="lower left")
 
 plt.show()
 
+print(50 * "=")
+print("Section: LDA via scikit-learn")
+print(50 * "-")
 
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel("LD 1")
+plt.ylabel("LD 2")
+plt.legend(loc="lower left")
+
+plt.show()
+
+X_test_lda = lda.transform(X_test_std)
+
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel("LD 1")
+plt.ylabel("LD 2")
+plt.legend(loc="lower left")
+
+plt.show()
+
+
+
+print(50 * "=")
+print("Section: Implementing a kernel principal component analysis in Python")
+print(50 * "-")
+
+def rbf_kernel_pca(X, gamma, n_components):
+  sq_dists = pdist(X, "sqeuclidean")
+
+  mat_sq_dists = squareform(sq_dists)
+
+  K = exp(-gamma * mat_sq_dists)
+
+  N = K.shape[0]
+  one_n = np.ones((N, N)) / N
+  K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+  eigvals, eigvecs = eigh(K)
+
+  X_pc = np.column_stack((eigvecs[:, -i]
+                          for i in range(1, n_components + 1)))
+
+  return X_pc
+
+
+
+print(50 * "=")
+print("Section: Example 1: Separating half-moon shapes")
+print(50 * "-")
+
+X, y = make_moons(n_samples=100, random_state=123)
+
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color="red", marker="^", alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color="blue", marker="o", alpha=0.5)
+
+plt.show()
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color="red", marker="^", alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color="blue", marker="o", alpha=0.5)
+
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50, 1)) + 0.02,
+              color="red", marker="^", alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50, 1)) - 0.02,
+              color="blue", marker="o", alpha=0.5)
+
+ax[0].set_xlabel("PC1")
+ax[0].set_ylabel("PC2")
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel("PC1")
+
+plt.show()
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((50, 1)) + 0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((50, 1)) - 0.02,
+              color='blue', marker='o', alpha=0.5)
+
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+
+# plt.tight_layout()
+# plt.savefig('./figures/half_moon_3.png', dpi=300)
+plt.show()
 
 
 
