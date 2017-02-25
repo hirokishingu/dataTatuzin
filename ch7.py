b7ca86353d895e46bd231e38d0c4b3184fe036e8
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-25 19:01:02 +0900

    アンサンブル分類器の評価とチューニング

--- ch7.py
+++ ch7.py
@@ -118,7 +118,7 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
     return self
 
   def predict(self, X):
-    if self.vote = "probability":
+    if self.vote == "probability":
       maj_vote = np.argmax(self.predict_proba(X), axis=1)
     else:
       predictions = np.asarray([clf.predict(X)
@@ -173,7 +173,7 @@ clf2 = DecisionTreeClassifier(max_depth=1,
 
 clf3 = KNeighborsClassifier(n_neighbors=1,
                             p=2,
-                            metrics="minlowski")
+                            metric="minkowski")
 
 pipe1 = Pipeline([["sc", StandardScaler()],
                   ["clf", clf1]])
@@ -206,6 +206,120 @@ for clf, label in zip(all_clf, clf_labels):
           % (scores.mean(), scores.std(), label))
 
 
+
+print(50 * '=')
+print('Section: Evaluating and tuning the ensemble classifier')
+print(50 * '-')
+
+colors = ["black", "orange", "blue", "green"]
+linestyles = [":", "--", "-.", "-"]
+for clf, label, clr, ls \
+        in zip(all_clf,
+               clf_labels, colors, linestyles):
+
+
+  y_pred = clf.fit(X_train,
+                  y_train).predict_proba(X_test)[:, 1]
+  fpr, tpr, thresholds = roc_curve(y_true=y_test,
+                                  y_score=y_pred)
+
+  roc_auc = auc(x=fpr, y=tpr)
+  plt.plot(fpr, tpr,
+            color=clr,
+            linestyle=ls,
+            label="%s (auc = %0.2f" % (label, roc_auc))
+
+plt.legend(loc="lower right")
+plt.plot([0, 1], [0, 1],
+        linestyle="--",
+        color="gray",
+        linewidth=2)
+
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid()
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+
+plt.show()
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                    np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=2, ncols=2,
+                        sharex="col",
+                        sharey="row",
+                        figsize=(7, 5))
+
+for idx, clf, tt in zip(product([0, 1], [0, 1]),
+                        all_clf, clf_labels):
+  clf.fit(X_train_std, y_train)
+
+  Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+  Z = Z.reshape(xx.shape)
+
+  axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+
+  axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 0, 0],
+                                X_train_std[y_train == 0, 1],
+                                c="blue",
+                                marker="^",
+                                s=50)
+
+  axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 1, 0],
+                                X_train_std[y_train == 1, 1],
+                                c="red",
+                                marker="o",
+                                s=50)
+  axarr[idx[0], idx[1]].set_title(tt)
+
+plt.text(-3.5, -4.5,
+          s="Sepal width [standardized]",
+          ha="center", va="center", fontsize=12)
+plt.text(-10.5, 4.5,
+        s="Petal length [standardized]",
+        ha="center", va="center",
+        fontsize=12, rotation=90)
+
+plt.show()
+
+print(mv_clf.get_params())
+
+params = {'decisiontreeclassifier__max_depth': [1, 2],
+          'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
+
+grid = GridSearchCV(estimator=mv_clf,
+                    param_grid=params,
+                    cv=10,
+                    scoring="roc_auc")
+grid.fit(X_train, y_train)
+
+if Version(sklearn_version) < "0.18":
+  for params, mean_score, scores in grid.grid_scores_:
+    print("%0.3f +/- %0.2f %r"
+            % (mean_score, scores.std() / 2.0, params))
+
+else:
+  cv_keys = ("mean_test_score", "std_test_score", "params")
+
+  for r, _ in enumerate(grid.cv_results_["mean_test_score"]):
+    print("%0.3f +/- %0.2f %r"
+          % (grid.cv_results_[cv_keys[0]][r],
+            grid.cv_results_[cv_keys[1]][r] / 2.0,
+            grid.cv_results_[cv_keys[2]][r]))
+
+print("Best parameters: %s" % grid.best_params_)
+print("Accuracy: %.2f" % grid.best_score_)
 
 
 
