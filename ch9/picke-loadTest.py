a15c3c3e07388bea8cb7b872f91e3471a193bed5
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-27 15:45:30 +0900

    見やすく変更

---
+++ ch9/picke-loadTest.py
@@ -0,0 +1,47 @@
+import pickle
+import re
+import os
+from vecto import vect
+import numpy as np
+
+clf = pickle.load(open("classifier.pkl", "rb"))
+
+label = {0: "negative", 1: "positive"}
+example = ["I love this movie"]
+
+X = vect.transform(example)
+
+print("Prediction: %s\nProbability: %.2f%%" %
+      (label[clf.predict(X)[0]],
+        np.max(clf.predict_proba(X)) * 100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
