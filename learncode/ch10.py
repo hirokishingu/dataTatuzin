e174222e1187164fa1a0bbf5b4d730523752ccf4
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-28 10:07:14 +0900

    ransac

--- learncode/ch10.py
+++ learncode/ch10.py
@@ -155,7 +155,45 @@ print("Intercept: %.3f" % w[0])
 
 
 
+print(50 * '=')
+print('Section: Fitting a robust regression model using RANSAC')
+print(50 * '-')
 
+
+if Version(sklearn_version) < '0.18':
+    ransac = RANSACRegressor(LinearRegression(),
+                             max_trials=100,
+                             min_samples=50,
+                             residual_metric=lambda x: np.sum(np.abs(x), axis=1),
+                             residual_threshold=5.0,
+                             random_state=0)
+else:
+    ransac = RANSACRegressor(LinearRegression(),
+                             max_trials=100,
+                             min_samples=50,
+                             loss='absolute_loss',
+                             residual_threshold=5.0,
+                             random_state=0)
+
+ransac.fit(X, y)
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+plt.scatter(X[inlier_mask], y[inlier_mask],
+            c="blue", marker="o", label="Inliers")
+plt.scatter(X[outlier_mask], y[outlier_mask],
+            c="lightgreen", marker="s", label="Outliers")
+plt.plot(line_X, line_y_ransac, color="red")
+plt.xlabel("Average number of rooms [RM]")
+plt.ylabel("Price in $1000\'s [MEDV]")
+plt.legend(loc="upper left")
+
+plt.show()
+
+print("Slope: %.3f" % ransac.estimator_.coef_[0])
+print("Intercept: %.3f" % ransac.estimator_.intercept_)
 
 
 
