98a41c6818acaa257779866651f9f940a6f32161
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-28 10:39:21 +0900

    R^2

--- learncode/ch10.py
+++ learncode/ch10.py
@@ -196,25 +196,187 @@ print("Slope: %.3f" % ransac.estimator_.coef_[0])
 print("Intercept: %.3f" % ransac.estimator_.intercept_)
 
 
+print(50 * '=')
+print('Section: Evaluating the performance of linear regression models')
+print(50 * '-')
+
+X = df.iloc[:, :-1].values
+y = df["MEDV"].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.3, random_state=0)
+
+slr = LinearRegression()
+
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+plt.scatter(y_train_pred, y_train_pred - y_train,
+            c="blue", marker="o", label="Training data")
+plt.scatter(y_test_pred, y_test_pred - y_test,
+            c="lightgreen", marker="s", label="Test data")
+plt.xlabel("Predicted values")
+plt.ylabel("Residuals")
+plt.legend(loc="upper left")
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color="red")
+plt.xlim([-10, 50])
+
+plt.show()
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
 
 
 
+print(50 * '=')
+print('Section: Using regularized methods for regression')
+print(50 * '-')
+
+print('LASSO Coefficients')
+
+lasso = Lasso(alpha=0.1)
+lasso.fit(X_train, y_train)
+y_train_pred = lasso.predict(X_train)
+y_test_pred = lasso.predict(X_test)
+print(lasso.coef_)
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
 
 
+print(50 * '=')
+print('Section: Turning a linear regression model into a curve'
+      ' - polynomial regression')
+print(50 * '-')
+
+X = np.array([258.0, 270.0, 294.0,
+              320.0, 342.0, 368.0,
+              396.0, 446.0, 480.0, 586.0])[:, np.newaxis]
+
+y = np.array([236.4, 234.4, 252.8,
+              298.6, 314.2, 342.2,
+              360.8, 368.0, 391.2,
+              390.8])
+
+lr = LinearRegression()
+pr = LinearRegression()
+quadratic = PolynomialFeatures(degree=2)
+X_quad = quadratic.fit_transform(X)
+
+lr.fit(X, y)
+X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+y_lin_fit = lr.predict(X_fit)
+
+pr.fit(X_quad, y)
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+
+plt.scatter(X, y, label="training points")
+plt.plot(X_fit, y_lin_fit, label="linear fit", linestyle="--")
+plt.plot(X_fit, y_quad_fit, label="quadratic fit")
+plt.legend(loc="upper left")
+
+plt.show()
+
+y_lin_pred = lr.predict(X)
+y_quad_pred = pr.predict(X_quad)
 
 
+print('Training MSE linear: %.3f, quadratic: %.3f' % (
+        mean_squared_error(y, y_lin_pred),
+        mean_squared_error(y, y_quad_pred)))
+print('Training R^2 linear: %.3f, quadratic: %.3f' % (
+        r2_score(y, y_lin_pred),
+        r2_score(y, y_quad_pred)))
 
 
+print(50 * '=')
+print('Section: Modeling nonlinear relationships in the Housing Dataset')
+print(50 * '-')
 
+X = df[["LSTAT"]].values
+y = df["MEDV"].values
 
+regr = LinearRegression()
 
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
 
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
 
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
 
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
 
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
 
+plt.scatter(X, y, label='training points', color='lightgray')
 
+plt.plot(X_fit, y_lin_fit,
+         label='linear (d=1), $R^2=%.2f$' % linear_r2,
+         color='blue',
+         lw=2,
+         linestyle=':')
 
+plt.plot(X_fit, y_quad_fit,
+         label='quadratic (d=2), $R^2=%.2f$' % quadratic_r2,
+         color='red',
+         lw=2,
+         linestyle='-')
+
+plt.plot(X_fit, y_cubic_fit,
+         label='cubic (d=3), $R^2=%.2f$' % cubic_r2,
+         color='green',
+         lw=2,
+         linestyle='--')
+
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.legend(loc='upper right')
+
+plt.show()
+
+print("Transforming the dataset")
+X = df[["LSTAT"]].values
+y = df["MEDV"].values
+
+X_log = np.log(X)
+y_sqrt = np.sqrt(y)
+
+X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+
+regr = regr.fit(X_log, y_sqrt)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+plt.scatter(X_log, y_sqrt, label="training points", color="lightgray")
+
+plt.plot(X_fit, y_lin_fit,
+          label="linear (d=1), $R^2=%.2f" % linear_r2,
+          color="blue",
+          lw=2)
+
+plt.xlabel("log(% lower status of the population [LSTAT])")
+plt.ylabel("$\sqrt{Price \; in \; \$1000\'s [MEDV]}$")
+plt.legend(loc="lower left")
+
+plt.show()
 
 
 
